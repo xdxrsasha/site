de4583d81511bb55a63a1938347f177ad85a66bb
xdxrsasha <krutkottt@gmail.com>
2025-11-28 10:11:03 +0200

    add pages folder and structure

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div style={{padding: '50px', fontFamily: 'sans-serif'}}>
+      <h1>Сайт запущен!</h1>
+      <p>Сейчас сюда подтянутся все твои компоненты.</p>
+    </div>
+  )
+}
