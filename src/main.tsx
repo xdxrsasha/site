0edcc9822f9ab21ce8c60cf067daf3aa1421d76e
xdxrsasha <krutkottt@gmail.com>
2025-11-28 10:14:28 +0200

    switch to Vite + React

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <div style={{padding: '50px', fontFamily: 'sans-serif'}}>
+      <h1>Сайт на Vite запущен!</h1>
+      <p>Сейчас подтянем твои компоненты</p>
+    </div>
+  </React.StrictMode>
+)
